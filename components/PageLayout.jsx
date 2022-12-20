850b716da034b904e670dd4c6495031b0e3dd762
Andres Maneiro <amaneiro7@gmail.com>
2022-12-20 02:17:17 -0400

    [UPDATE] optimizacion de imagenes y add de serversideprops

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default  function PageLayout({ children, title = 'NewsApp' }) {
     return (
@@ -9,13 +10,20 @@ export default  function PageLayout({ children, title = 'NewsApp' }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <header>
-                newssapi
+                <div>
+                    <Link href='/'>newssapi</Link>
+                </div>
+                <div>
+                    <Link href='/about'>About</Link>
+                </div>
             </header>
             <main>
                 {children}
             </main>
             <style jsx>{`
                 header {
+                    display: flex;
+                    justify-content: space-between;
                     padding: 20px;
                 }
             `}</style>
--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,57 @@ import styles from '../styles/Home.module.css'
 import PageLayout from '../components/PageLayout';
 import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const [articles, setArticles] = useState([])
-  const API_KEY = '24434dd522bb4d23bf73c8068f723146'
+export default function Home({ articles }) {
+  return (    
+    <PageLayout title='NewsApp - Home'>
+      <div className={styles.container}>
+        {articles.length === 0 && <p>No tenemos articulos</p>}
+        {articles.length> 0 && articles.map((article, index) => (
+          <article key={index}>
+            <Image 
+              src={article.urlToImage} 
+              alt={`Image for the article ${article.title}`}
+              width={450}
+              height={350}
+              quality={50}
+              layout='responsive'
+              priority={index < 2}              
+            />
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+          </article>
+        ))}
+      </div>
+    </PageLayout>    
+  );
+};
 
-  useEffect(() => {
-    fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=24434dd522bb4d23bf73c8068f723146')
-      .then(res => res.json())
-      .then(response => {
-        const {articles} = response
-        setArticles(articles)        
-      })
-    }, [])
-  
-    return (    
-      <PageLayout title='NewsApp - Home'>
-        <div className={styles.container}>
-          {articles.length === 0 && <p>Loading ...</p>}
-          {articles.length> 0 && articles.map((article, index) => (
-            <article key={index}>
-              <Image 
-                src={article.urlToImage} 
-                alt={`Image for the article ${article.title}`}
-                width={auot}
-                height={auto}
-              />
-              <h2>{article.title}</h2>
-              <p>{article.description}</p>
-            </article>
-          ))}
-        </div>
-      </PageLayout>    
-  )
-}
+// N requets -> se ejecuta 1 vez en build time (o para refrescar la pagina)
+
+// export async function getStaticProps() {
+//   const API_KEY = '24434dd522bb4d23bf73c8068f723146';
+//   (`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`)
+//   const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`)
+//   const { articles } = await response.json()
+//   return {
+//     props: {
+//       articles
+//     }
+//   };
+// };
+
+// N request -> se ejecuta N veces
+// para datos que necesitas que sean MUY live
+// tiene demasiados datos dinamicos
+
+export async function getServerSideProps(context) {
+  const API_KEY = '24434dd522bb4d23bf73c8068f723146';
+  (`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`)
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`)
+  const { articles } = await response.json()
+  return {
+    props: {
+      articles
+    }
+  };
+};
